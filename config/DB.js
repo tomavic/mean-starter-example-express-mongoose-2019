dcb697d792398feaf9df3f0662989efe8374d8ea
tomavic <hbasheer@live.com>
2019-03-07 15:07:57 +0200

    ...

--- config/DB.js
+++ config/DB.js
@@ -5,5 +5,5 @@ const password = 'tomas%40admin19';
 
 module.exports = {
   DB: `mongodb://localhost/resthub`,
-  liveDB: `mongodb+srv://${username}:${password}@cluster0-liccb.mongodb.net/test?retryWrites=true`
+  liveDB: `mongodb+srv://tomas:tomas@admin19@cluster0-liccb.mongodb.net/test?retryWrites=true`
 };
