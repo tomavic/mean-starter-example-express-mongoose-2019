55521b7345c7455c63618dba3cf4b444755d1109
tomavic <hbasheer@live.com>
2019-03-20 22:12:39 +0200

    holaaaz

--- config/DB.js
+++ config/DB.js
@@ -1,9 +1,9 @@
 // DB.js
 const username = 'boy';
-const password = 'hn9MmEUHgLNmUVU6';
+const password = 'VZOAjkMmFv00Lcn2';
 
 
 module.exports = {
   DB: `mongodb://localhost/resthub`,
-  liveDB: `mongodb://boy:44eSydrtMsb7zArx@pm-shard-00-00-liccb.mongodb.net:27017,pm-shard-00-01-liccb.mongodb.net:27017,pm-shard-00-02-liccb.mongodb.net:27017/test?ssl=true&replicaSet=PM-shard-0&authSource=admin&retryWrites=true`
+  liveDB: `mongodb+srv://boy:VZOAjkMmFv00Lcn2@pm-liccb.mongodb.net/test?retryWrites=true`
 };
